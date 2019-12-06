d6cd454ef993f33f4b5194f5cdf31dfdd69ea1fb
Weslley Araujo <weslley.araujo@booking.com>
2019-12-06 11:56:05 +0100

    fix(airtable): parse properly strings and json

--- scripts/fetch-airtable-db.js
+++ scripts/fetch-airtable-db.js
@@ -7,30 +7,65 @@ const base = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY
 }).base(process.env.AIRTABLE_BASE);
 
-base("Artists")
-  .select({
-    view: "Grid view"
-  })
-  .all(
-    (_, records) => {
-      const data = records.map(item => {
-        return Object.entries(item.fields).reduce(
-          (acc, [key, value]) => ({
-            ...acc,
-            [key.toLowerCase()]: value
-          }),
-          {
-            id: item.id
-          }
-        );
-      });
+function fetchBase(name, view = "Grid view", fileName) {
+  console.log(`Fetching: "${name}"...`);
+  return new Promise((resolve, reject) => {
+    base(name)
+      .select({
+        view
+      })
+      .all(
+        (_, records) => {
+          const data = records.map(item => {
+            return Object.entries(item.fields).reduce(
+              (acc, [field, content]) => {
+                const key = field.toLowerCase();
+                const value = key === "cover" ? content[0].url : content;
+                switch (typeof value) {
+                  case "string": {
+                    return {
+                      ...acc,
+                      [key]: String(
+                        key === "cover" ? content[0].url : content
+                      ).trim()
+                    };
+                  }
+                  default: {
+                    return {
+                      ...acc,
+                      [key]: content
+                    };
+                  }
+                }
+              },
+              {
+                id: item.id
+              }
+            );
+          });
 
-      fs.writeFileSync(
-        path.join(__dirname, "../src/data/artists.json"),
-        JSON.stringify(data, null, 2)
+          const output = `../src/data/${fileName}.json`;
+          fs.writeFileSync(
+            path.join(__dirname, output),
+            JSON.stringify(data, null, 2)
+          );
+          console.log(`Content for "${name}" saved at ${output} \n`);
+          resolve({});
+          return;
+        },
+        err => {
+          if (err) {
+            reject(err);
+          }
+        }
       );
-    },
-    err => {
-      console.error(err);
-    }
-  );
+  });
+}
+
+async function run() {
+  await fetchBase("Artists", "Grid view", "artists");
+  await fetchBase("Albums", "Grid view", "albums");
+  await fetchBase("Tracks", "Grid view", "tracks");
+}
+
+run();
